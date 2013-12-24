1f2f20979ca7293488fe5246c98b06092719147f
kikimosha <eszmvcu@mail.ru>
2013-12-24 21:31:39 +0200

    fix js after merge

--- js/profile_tabs.js
+++ js/profile_tabs.js
@@ -1,13 +1,22 @@
 (function($){
-
     $(document).ready(function() {
+
+        // tabs
         $( "#tabs" ).tabs({
             active: 7,
             activate: function( event, ui ) {
-                $(ui.newTab).find('a').addClass('active');
-                $(ui.oldTab).find('a').removeClass('active');
+                $(ui.newTab).addClass('active');
+                $(ui.oldTab).removeClass('active');
             }
         });
+
+        // shop_categories
+        $('#shop_categories').accordion({
+            collapsible: true,
+            active: 0,
+            heightStyle: "content"
+        });
+
         $( "#select_bill" ).selectable();
     });
 })(jQuery);
